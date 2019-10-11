28af4e005c689404c96dd255f9cad0e511feb6b0
Mikhail Samoylov <mikhail.samoylov@percona.com>
2019-10-11 17:38:01 +0300

    Changes in tests for new package version.

--- molecule/pg-11/molecule/default/tests/settings.py
+++ molecule/pg-11/molecule/default/tests/settings.py
@@ -1,6 +1,6 @@
-DEB_PKG_VERSIONS = ["11+204-1.buster", "204-1.buster", "1:11-5.buster", "1:11-5.stretch", "204-1.stretch",
-                    "11+204-1.stretch", "1:11-5.bionic", '11+204-1.bionic', "204-1.bionic", "1:11-5.cosmic",
-                    "11+204-1.cosmic", "204-1.cosmic", "1:11-5.disco", "11+204-1.disco", "204-1.disco"]
+DEB_PKG_VERSIONS = ["11+204-2.buster", "204-2.buster", "2:11-5.2.buster", "2:11-5.2.stretch", "204-2.stretch",
+                    "11+204-2.stretch", "2:11-5.2.bionic", '11+204-2.bionic', "204-2.bionic", "2:11-5.2.cosmic",
+                    "11+204-2.cosmic", "204-2.cosmic", "2:11-5.2.disco", "11+204-2.disco", "204-2.disco"]
 
 DEB_PACKAGES = ["percona-postgresql-11", "percona-postgresql-client", "percona-postgresql",
                 "percona-postgresql-client-11", "percona-postgresql-client-common",
@@ -35,14 +35,14 @@ DEB_FILES = ["/etc/postgresql/11/main/postgresql.conf", "/etc/postgresql/11/main
 RHEL_FILES = ["/var/lib/pgsql/11/data/postgresql.conf", "/var/lib/pgsql/11/data/pg_hba.conf",
               "/var/lib/pgsql/11/data/pg_ident.conf"]
 
-EXTENSIONS = ['xml2', 'tcn', 'plpythonu', 'plpython3u', 'plpython2u', 'pltcl', 'hstore', 'plperlu', 'plperl', 'ltree',
+EXTENSIONS = ['xml2', 'tcn', 'plpython3u', 'pltcl', 'hstore', 'plperlu', 'plperl', 'ltree',
               'hstore_plperlu', 'dict_xsyn', 'autoinc', 'hstore_plpython3u','insert_username', 'intagg', 'adminpack',
-              'intarray', 'cube', 'lo', 'jsonb_plpython2u', 'jsonb_plperl', 'jsonb_plperlu', 'btree_gin', 'pgrowlocks',
-              'bloom', 'seg', 'pageinspect', 'btree_gist', 'sslinfo', 'pg_visibility', 'ltree_plpython2u', 'refint',
-              'jsonb_plpython3u', 'jsonb_plpythonu', 'moddatetime', 'ltree_plpythonu', 'dict_int', 'pg_freespacemap',
-              'pgstattuple', 'hstore_plpythonu', 'uuid-ossp', 'tsm_system_time', 'tsm_system_rows', 'unaccent',
+              'intarray', 'cube', 'lo', 'jsonb_plperl', 'jsonb_plperlu', 'btree_gin', 'pgrowlocks',
+              'bloom', 'seg', 'pageinspect', 'btree_gist', 'sslinfo', 'pg_visibility', 'refint',
+              'jsonb_plpython3u', 'moddatetime', 'dict_int', 'pg_freespacemap',
+              'pgstattuple', 'uuid-ossp', 'tsm_system_time', 'tsm_system_rows', 'unaccent',
               'tablefunc', 'pgcrypto', 'pg_buffercache', 'amcheck', 'citext',  'timetravel',  'isn',
-              'hstore_plpython2u', 'ltree_plpython3u', 'fuzzystrmatch', 'earthdistance', 'hstore_plperl', 'pg_prewarm',
+              'ltree_plpython3u', 'fuzzystrmatch', 'earthdistance', 'hstore_plperl', 'pg_prewarm',
               'dblink', 'pltclu', 'file_fdw', 'pg_stat_statements', 'postgres_fdw']
 
-LANGUAGES = ["pltcl", "pltclu", "plperl", "plperlu", "plpythonu", "plpython2u", "plpython3u"]
+LANGUAGES = ["pltcl", "pltclu", "plperl", "plperlu", "plpython3u"]
